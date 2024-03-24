c1cb4eb79eec44387413582bfe058f925c8a0002
Josef Mana <josef.mana@protonmail.com>
2024-03-24 21:54:16 +0100

    graphical abstract figure finished, it is beautiful now

--- scripts/graphstract.R
+++ scripts/graphstract.R
@@ -7,14 +7,14 @@ s = 87542 # seed for reproducibility
 library(here) # directory management
 library(tidyverse) # data wrangling
 library(tidybayes) # posterior manipulation
-library(patchwork) # figures
 
 
 # LOAD MATERIALS ----
 
 for ( i in names( readRDS( here("_data","longitudinal_df.rds") ) ) ) assign( i, readRDS( here("_data","longitudinal_df.rds") )[[i]] ) # load all the data
 m <- readRDS( here("mods","m1_lasso_doms.rds") ) # load the model
-prds <- c("exec_fun","epis_mem") # list of predictors effect which we're going to visualize
+prds <- doms
+#prds <- c("exec_fun","epis_mem") # list of predictors effect which we're going to visualize
 
 
 # PRE-PROCESSING ----
@@ -43,7 +43,7 @@ d <-
 # POSTERIOR PREDICTION ----
 
 # takes a lot of time to compute so load if the predictions already exist
-if ( !file.exists( here("_data","graphstract_epred.rds") ) ) {
+if ( !file.exists( here("_data","graphstract_epred.csv") ) ) {
   
   # prepare a prediction dummy data set
   d_seq <-
@@ -80,73 +80,108 @@ if ( !file.exists( here("_data","graphstract_epred.rds") ) ) {
         
         # add predictions of the expectation (e_pred) to d_seq
         add_epred_draws( m , allow_new_levels = T, seed = s, if (i == "epred_fix") { re_formula = NA } ) %>%
-        mutate(.epred = scl$M$drs + scl$SD$drs * .epred) %>%
-        median_hdi(.width = .95)
+        mutate(.epred = scl$M$drs + scl$SD$drs * .epred ) %>%
+        median_hdi(.width = .95) %>%
+        mutate( predictor = j, type = i )
       
     } 
   }
   
-  # save it
-  saveRDS( d_seq, here("_data","graphstract_epred.rds") )
+  # pull it all together
+  d_seq <-
+    
+    do.call(
+      rbind.data.frame,
+      list(
+        do.call( rbind.data.frame, d_seq$epred_fix ),
+        do.call( rbind.data.frame, d_seq$epred_all )
+      )
+    )
   
+  # save it
+  #saveRDS( d_seq, here("_data","graphstract_epred.rds") )
+  write.table( d_seq, file = here("_data","graphstract_epred.csv"), sep = ",", row.names = F, quote = F )
+
 } else {
   
   # if it was calculated in past, just load it
-  d_seq <- readRDS( here("_data","graphstract_epred.rds") )
+  #d_seq <- readRDS( here("_data","graphstract_epred.rds") )
+  d_seq <- read.csv( here("_data","graphstract_epred.csv"), sep = "," )
   
 }
 
 
 # PLOTTING ----
 
-# plot facets' names
-nms <-
-  list(
-    exec_fun = bquote("Longitudinal cognition predicted by pre-surgery" ~bold("executive functions") ),
-    epis_mem = bquote("Longitudinal cognition predicted by pre-surgery" ~bold("episodic memory") )
-  )
+# set-up colors (grouped according to cognitive functions)
+clr <- c( "#78A641", "#8A60B0", "#2CA030", "#12A2A8", "#FF7F0E", "#6F63BB", "#1F83B4" )
+
+# extract order of variables from the most to the least predictive
+ord <-
+  
+  m %>%
+  spread_draws( `b_.*`, regex = T ) %>%
+  select( contains("time:") ) %>%
+  rename_with( ~ sub("b_time:","",.x) ) %>%
+  apply( 2, median ) %>%
+  sort() %>%
+  names()
+
+# make predictor an ordered factor in posterior predictive dataset
+d_seq <- d_seq %>% mutate( predictor = factor( predictor, levels = ord, ordered = T ) )
 
 # prepare figure for the graphical abstract
-f <-
+d %>%
   
-  lapply(
-    
-    setNames(prds,prds),
-    function(i)
-      
-      d[ , c( "time_y","id","drs") ] %>%
-      mutate( grp = d[ , paste0(i, "_pent") ] ) %>%
-      ggplot( aes(x = time_y, y = drs, group = id) ) +
-      geom_hline( yintercept = 139, linetype = "dotted", linewidth = 1.5, alpha = 1 ) +
-      geom_line( linewidth = .5, alpha = .33 ) +
-      geom_point( size = 4, alpha = .33 ) +
-      geom_ribbon(data = d_seq$epred_all[[i]], alpha = .1, aes(x = time_y, y = .epred, ymin = .lower, ymax = .upper, fill = grp ) ) +
-      geom_ribbon(data = d_seq$epred_fix[[i]], alpha = .3, aes(x = time_y, y = .epred, ymin = .lower, ymax = .upper, fill = grp ) ) +
-      geom_line(data = d_seq$epred_fix[[i]], linewidth = 2.5, aes( x = time_y, y = .epred, color = grp ) ) +
-      scale_color_gradient( low = "red", high = "grey66" ) +
-      scale_y_continuous(name = "DRS-2", limits = c(80,153), breaks = seq(80,140,20), labels = seq(80,140,20) ) +
-      scale_x_continuous(name = "Time from surgery (years)", limits = c(-3,12), breaks = seq(-2,12,2), labels = seq(-2,12,2) ) +
-      facet_wrap(
-        ~  grp,
-        nrow = 1,
-        labeller = as_labeller( c(`1` = "first pentile",
-                                  `2` = "second pentile",
-                                  `3` = "third pentile",
-                                  `4` = "fourth pentile",
-                                  `5` = "fifth pentile"
-                                  )
-                                )
-      ) +
-      ggtitle( nms[[i]] ) +
-      theme_minimal(base_size = 22) +
-      theme( legend.position = "none", plot.title = element_text(hjust = .5) )
-      
+  # prepare data
+  pivot_longer(
+    cols = ends_with("pent"),
+    names_to = "predictor",
+    names_transform = function(x) sub("_pent","",x), values_to = "grp"
+  ) %>%
+  mutate( predictor = factor( predictor, levels = ord, ordered = T ) ) %>%
+  
+  # plot raw data
+  ggplot( aes(x = time_y, y = drs, group = id) ) +
+  geom_hline( yintercept = 139, linetype = "dotted", linewidth = 1, alpha = 1 ) +
+  geom_line( linewidth = .5, alpha = .33 ) +
+  geom_point( size = 2.5, alpha = .33 ) +
+  
+  # plot posterior predictions
+  geom_ribbon( data = subset( d_seq, type == "epred_all" ), alpha = .2, aes(x = time_y, y = .epred, ymin = .lower, ymax = .upper, fill = predictor ) ) +
+  geom_ribbon( data = subset( d_seq, type == "epred_fix" ), alpha = .3, aes(x = time_y, y = .epred, ymin = .lower, ymax = .upper, fill = predictor ) ) +
+  geom_line( data = subset( d_seq, type == "epred_fix" ), linewidth = 1, aes( x = time_y, y = .epred, colour = predictor ) ) +
+  
+  # tidy-up axes and colours
+  scale_y_continuous(name = "DRS-2", limits = c(80,153), breaks = seq(80,140,20), labels = seq(80,140,20) ) +
+  scale_x_continuous(name = "Time from surgery (years)", limits = c(-2,12), breaks = seq(-2,12,2), labels = seq(-2,12,2) ) +
+  scale_colour_manual( values = clr ) +
+  scale_fill_manual( values = clr ) +
+  
+  # final touches
+  theme_minimal( base_size = 16 ) +
+  theme( legend.position = "none" ) +
+  facet_grid(
+    rows = vars(predictor),
+    cols = vars(grp),
+    labeller =
+      as_labeller(
+        c( `1` = "1^th~pentile",
+           `2` = "2^nd~pentile",
+           `3` = "3^rd~pentile",
+           `4` = "4^th~pentile",
+           `5` = "5^th~pentile",
+           exec_fun = "Executive~functions",
+           visp_mem = "Visuospatial~memory",
+           set_shift = "Set~shifting",
+           epis_mem = "Episodic~memory",
+           anxiety = "Anxiety",
+           visp_wm = "Spatial~working~memory",
+           verb_wm = "Verbal~working~memory"
+        ),
+        label_parsed
+      )
   )
 
-
-# arrange figure's subplots for saving
-( f$exec_fun / f$epis_mem )
-
 # save the figure
-ggsave( here("figs","graphstract_epred.jpg"), dpi = 300, width = 1.5 * 13.1, height = 14.4 )
-
+ggsave( here("figs","graphstract_epred.jpg"), dpi = 300, width = 12.3, height = 14.7 )

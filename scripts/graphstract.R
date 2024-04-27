4e5a6b57c5189d77adc0e3e930633090ab8b6271
Josef Mana <josef.mana@protonmail.com>
2024-04-27 21:28:46 +0200

    re-formatting figures/tables names, finishing the manuscript before submission, starting to prepare ReadMe file

--- scripts/graphstract.R
+++ scripts/graphstract.R
@@ -5,18 +5,21 @@ rm( list = ls() ) # clear environment
 s = 87542 # seed for reproducibility
 
 library(here) # directory management
+library(patchwork) # putting graphs together
 library(tidyverse) # data wrangling
 library(tidybayes) # posterior manipulation
 
 
-# LOAD MATERIALS ----
+# POSTERIOR PREDICTION ----
+
+## LOAD MATERIALS ----
 
 for ( i in names( readRDS( here("_data","longitudinal_df.rds") ) ) ) assign( i, readRDS( here("_data","longitudinal_df.rds") )[[i]] ) # load all the data
 m <- readRDS( here("mods","m1_lasso_doms.rds") ) # load the model
 prds <- doms
 
 
-# PRE-PROCESSING ----
+## PRE-PROCESSING ----
 
 # prepare data set with patients' median pre-surgery cognitive profile across imputations
 d <-
@@ -39,7 +42,7 @@ d <-
   )
 
 
-# POSTERIOR PREDICTION ----
+## POSTERIOR PREDICTION ----
 
 # takes a lot of time to compute so load if the predictions already exist
 if ( !file.exists( here("_data","graphstract_epred.csv") ) ) {
@@ -108,10 +111,10 @@ if ( !file.exists( here("_data","graphstract_epred.csv") ) ) {
 }
 
 
-# PLOTTING ----
+## PLOTTING ----
 
-# set-up colors (grouped according to cognitive functions)
-clr <- c( "#78A641", "#8A60B0", "#2CA030", "#12A2A8", "#FF7F0E", "#6F63BB", "#1F83B4" )
+theme_set( theme_minimal( base_size = 14 ) ) # set-up theme
+clr <- c( "#78A641", "#8A60B0", "#2CA030", "#FF7F0E", "#12A2A8", "#6F63BB", "#1F83B4" ) # set-up colors (grouped according to cognitive functions)
 
 # extract order of variables from the most to the least predictive
 ord <-
@@ -128,9 +131,11 @@ ord <-
 d_seq <- d_seq %>% mutate( predictor = factor( predictor, levels = ord, ordered = T ) )
 
 # prepare figure for the graphical abstract
-d %>%
+
+f3 <-
   
   # prepare data
+  d %>%
   pivot_longer(
     cols = ends_with("pent"),
     names_to = "predictor",
@@ -151,13 +156,12 @@ d %>%
   
   # tidy-up axes and colours
   scale_y_continuous( name = "DRS-2", limits = c(80,155), breaks = seq(80,140,20), labels = seq(80,140,20) ) +
-  scale_x_continuous( name = "Time from surgery (years)", limits = c(-2,12), breaks = seq(-2,12,2), labels = seq(-2,12,2) ) +
+  scale_x_continuous( name = "Time post-surgery (years)", limits = c(-2,12), breaks = seq(-2,12,2), labels = seq(-2,12,2) ) +
   scale_colour_manual( values = clr ) +
   scale_fill_manual( values = clr ) +
   
   # final touches
-  theme_minimal( base_size = 16 ) +
-  theme( legend.position = "none" ) +
+  theme( legend.position = "none", panel.grid.minor = element_blank() ) +
   facet_grid(
     rows = vars(predictor),
     cols = vars(grp),
@@ -181,4 +185,120 @@ d %>%
   )
 
 # save the figure
-ggsave( here("figs","graphstract_epred.jpg"), dpi = 300, width = 12.3, height = 14.7 )
+ggsave( plot = f3, here("figs","pred_stratified.jpg"), dpi = 300, width = 11.1, height = 13.3 )
+
+
+# DESCRIPTION ----
+
+# read the data set and prepare subsets for individual analyses
+d1 <- read.csv( here("_data","20220508_dbs_longCOG_data.csv") , sep = "," ) %>% filter( included == 1 )
+
+# plot it
+f1 <-
+  
+  d1[ complete.cases(d1$drs_tot) , ] %>% 
+  ggplot( aes(x = time_y) ) +
+  stat_bin( breaks = seq(-2,12,.5) ) + # creates bars
+  stat_bin( breaks = seq(-2,12,.5), geom = "text", aes(label = after_stat(count) ), vjust = -0.8, size = 3.5 ) + # add numbers
+  labs( x = "Time post-surgery (years)", y = "Number of Assessments" ) +
+  scale_y_continuous( expand = c(0, 0), limits = c(0, 107), breaks = seq(0, 100, 10), labels = seq(0, 100, 10) ) +
+  scale_x_continuous( limits = c(-2, 12), breaks = seq(-2, 12, 1), labels = seq(-2, 12, 1) ) +
+  theme( panel.grid.minor = element_blank() )
+
+
+# CONTRASTS ----
+
+# prepare colors to use in graphs (a colorblind-friendly palette)
+cbPal <- c( "#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7" )
+
+## PREPARE DATA ----
+d_seq <-
+  
+  data.frame( id = "sub000", time_y = c( -.3, seq(.5,5,1/12) ) ) %>%
+  mutate( time = time_y + scl$Md$time )
+
+# contrast for the true score
+contr <-
+  
+  lapply(
+    
+    c("fixed","random"),
+    function(i)
+      
+      # extract predictions
+      d_seq %>%
+      add_epred_draws(
+        object = readRDS( here("mods","m0_linear.rds") ),
+        newdata = . ,
+        allow_new_levels = T,
+        if ( i == "fixed" ) { re_formula = NA },
+        seed = 87542
+      ) %>%
+      
+      # post-process
+      mutate( pred = scl$M$drs + scl$SD$drs * .epred ) %>%
+      mutate( pred = case_when( pred > 144 ~ 144, pred < 0 ~ 0, .default = pred) ) %>%
+      ungroup() %>%
+      select( id, time_y, .draw, pred ) %>%
+      pivot_wider( names_from = time_y, values_from = pred ) %>%
+      mutate( across( all_of( as.character( d_seq$time_y ) ), ~ .x-`-0.3` ) ) %>%
+      pivot_longer( cols = -c("id",".draw"), names_to = "contrast" ) %>%
+      group_by(contrast) %>%
+      summarise( md = median(value), eti_low = qi(value,.width=.9)[1], eti_hig = qi(value,.width=.9)[2] ) %>%
+      mutate( model = i, .before = 1 )
+    
+  )
+
+# extract predictive contrast of one year post-minus-pre assessments
+contr$all <-
+  
+  d_seq %>%
+  add_predicted_draws( object = readRDS( here("mods","m0_linear.rds") ), newdata = . , allow_new_levels = T, re_formula = NA, seed = 87542 ) %>%
+  mutate( pred = scl$M$drs + scl$SD$drs * .prediction ) %>%
+  mutate( pred = case_when( pred > 144 ~ 144, pred < 0 ~ 0, .default = pred ) ) %>%
+  ungroup() %>%
+  select( id, time_y, .draw, pred ) %>%
+  pivot_wider( names_from = time_y, values_from = pred ) %>%
+  mutate( across( all_of( as.character( d_seq$time_y ) ), ~ .x - `-0.3` ) ) %>%
+  pivot_longer( cols = -c("id",".draw"), names_to = "contrast" ) %>%
+  group_by(contrast) %>%
+  summarise( md = median(value), eti_low = qi(value,.width=.9)[1], eti_hig = qi(value,.width=.9)[2] ) %>%
+  mutate( model = "all", .before = 1 )
+
+# put it together
+contr <-
+  
+  contr %>%
+  do.call( rbind.data.frame, . ) %>%
+  filter( !grepl("-",contrast) ) %>%
+  pivot_wider( names_from = model, values_from = c("md","eti_low","eti_hig") ) %>%
+  mutate( contrast = as.numeric(contrast) )
+
+
+## PLOT IT ----
+f2 <-
+  
+  contr %>%
+  ggplot() +
+  aes( y = contrast, x = md_fixed ) +
+  
+  # add lines and shades
+  geom_ribbon( data = contr, aes(xmin = eti_low_all, xmax = eti_hig_all), fill = cbPal[8], alpha = .4 ) +
+  geom_ribbon( data = contr, aes(xmin = eti_low_random, xmax = eti_hig_random), fill = cbPal[8], alpha = .6 ) +
+  geom_ribbon( data = contr, aes(xmin = eti_low_fixed, xmax = eti_hig_fixed), fill = cbPal[8], alpha = 1 ) +
+  geom_line( size = 2, colour = "black" ) +
+  
+  # finish it
+  geom_vline( xintercept = 0, linewidth = 1, linetype = "dashed", colour = "navyblue" ) +
+  labs( y = "Time post-surgery (years)", x = parse( text = "Delta~`DRS-2`" ) ) +
+  scale_x_continuous( breaks = seq(-15,5,1), labels = seq(-15,5,1) ) +
+  coord_flip( xlim = c(-14,3) )
+
+# save it
+ggsave( plot = f2, file = here("figs","desc_contrasts.jpg"), dpi = 300, width = 9.66, height = 5.5 )
+
+
+# MERGING ----
+
+( (f1/f2) | f3 ) + plot_layout(widths = c(1,1.25) )
+ggsave( plot = last_plot(), file = here("figs","graphstract.jpg"), dpi = 300, width = 18.3, height = 13.3 )
